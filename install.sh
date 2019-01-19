65c4edc00de58fa28b88ce67760d3480a5ef1600
it-toppp <36742579+it-toppp@users.noreply.github.com>
2019-01-19 19:10:14 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ apt-get install -y unattended-upgrades
 # If you'd rather inspect and install these files yourself, see:
 # https://docs.bytemark.co.uk/article/wordpress-on-docker-with-phpmyadmin-ssl-via-traefik-and-automatic-updates/#look-a-bit-deeper
 mkdir -p /root/compose
-curl -fsSL https://raw.githubusercontent.com/BytemarkHosting/configs-wordpress-docker/master/docker-compose.yml -o /root/compose/docker-compose.yml
+curl -fsSL https://raw.githubusercontent.com/it-toppp/configs-wordpress-docker/master/docker-compose.yml -o /root/compose/docker-compose.yml
 curl -fsSL https://raw.githubusercontent.com/BytemarkHosting/configs-wordpress-docker/master/.env -o /root/compose/.env
 curl -fsSL https://raw.githubusercontent.com/BytemarkHosting/configs-wordpress-docker/master/traefik.toml -o /root/compose/traefik.toml
 curl -fsSL https://raw.githubusercontent.com/BytemarkHosting/configs-wordpress-docker/master/php.ini -o /root/compose/php.ini
