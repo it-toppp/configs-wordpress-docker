113967fcc0979a1460a87eacc3f18811c058f207
it-toppp <36742579+it-toppp@users.noreply.github.com>
2019-01-22 10:02:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ while ! apt-get -qq check; do sleep 1s; done
 apt-get update
 apt-get install -y mc apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
+#add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#apt-get update
 #apt-get install -y docker-ce
-#curl -fsSL https://get.docker.com/ | sh
+curl -fsSL https://get.docker.com/ | sh
 curl -fsSL https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
  
