d3f06155ddfc436249b731a4cdfbf72378e3591f
it-toppp <36742579+it-toppp@users.noreply.github.com>
2019-01-22 15:27:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ while ! apt-get -qq check; do sleep 1s; done
 # Install docker-ce and docker-compose.
 apt-get update
 apt-get install -y mc apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 #add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 #apt-get update
 #apt-get install -y docker-ce
