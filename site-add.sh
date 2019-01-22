d77316fca4b5119d4fb2587a2dbf5229b0c531a9
it-toppp <36742579+it-toppp@users.noreply.github.com>
2019-01-22 12:22:04 +0300

    Update site-add.sh

--- site-add.sh
+++ site-add.sh
@@ -3,21 +3,19 @@ read HOST
 what is the site number
 echo "what is the site number: "
 read SNUM
-sed -i -e "s|^WORDPRESS_DB_NAME$SNUM=.*|WORDPRESS_DB_PASSWORD=" /root/compose/.env
-echo "WORDPRESS_DB_NAME$SNUM"
 #WORDPRESS_DB=wordpres$(cat /dev/urandom | tr -dc '[:digit:]' | head -c5`)
-
-cat >/root/compose/sql <<EOL
-CREATE DATABASE $wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
-FLUSH PRIVILEGES;
-EOL
 
 cat >/root/compose/.env <<EOL
 WORDPRESS_DB_NAME$SNUM=wordpres$SNUM
 WORDPRESS_DOMAINS$SNUM=$HOST
 
-docker exec -it compose_wp-db_1 mysql -u root -ppassword  -e "$(cat /root/sql)"
+cat >/root/compose/sql <<EOL
+CREATE DATABASE wordpress$SNUM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
+FLUSH PRIVILEGES;
+EOL
+
+docker exec -it compose_wp-db_1 mysql -u root -ppassword  -e "$(cat /root/compose/sql)"
 
 cat >>/root/compose/docker-compose.yml <<EOL
   wp$SNUM:
@@ -29,7 +27,7 @@ cat >>/root/compose/docker-compose.yml <<EOL
       - backend
       - frontend
     volumes:
-       - /var/www/htlm-2:/var/www/html
+       - /var/www/htlm/root/compose/sql:/var/www/html
      # - ./php.ini:/usr/local/etc/php/php.ini
     environment:
       WORDPRESS_DB_HOST: wp-db:3306
@@ -39,10 +37,11 @@ cat >>/root/compose/docker-compose.yml <<EOL
     labels:
       - "traefik.docker.network=frontend"
       - "traefik.enable=true"
-      - "traefik.frontend.rule=Host:${WORDPRESS_DOMAINS2}"
+      - "traefik.frontend.rule=Host:${WORDPRESS_DOMAINS$SNUM}"
       - "traefik.port=80"
       - "traefik.protocol=http"
 EOL
+mkdir -p /root/compose/www/html$SNUM
 cd /root/compose
 docker-compose up -d wp$SNUM
 
